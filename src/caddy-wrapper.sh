d39ab334a4ed95df78dd3ba148e6ef7161a252a0
Dilyn Corner <dilyn.corner@canonical.com>
2025-03-25 17:41:43 -0400

    caddy-wrapper.sh: split add|remove-package logic
    This setup should fully support adding (and removing) officially
    supported modules both at run-time and at initial install time.
    
    Signed-off-by: Dilyn Corner <dilyn.corner@canonical.com>

--- src/caddy-wrapper.sh
+++ src/caddy-wrapper.sh
@@ -3,21 +3,45 @@
 cli() {
   # Grab the first argument and drop it from the input
   arg="$1"
+  shift 1
 
   case $arg in
     # Users can update caddy with additional plugins or modules at runtime. The
     # caddy binary needs to be mutable in this case.
-    add-package|remove-package)
+    add-package)
       [ -e "${SNAP_COMMON}/caddy-mod" ] || {
         cp -f "${SNAP}/usr/bin/caddy" "${SNAP_COMMON}/caddy-mod"
         _caddy="${SNAP_COMMON}/caddy-mod"
       }
 
-      shift 1
       $_caddy "$arg" "$@"
 
-      snapctl set modified=true ;;
-    *) $_caddy "$@"             ;;
+      for mod in "$@"; do
+        snapctl set "module.${mod##*/}"="$mod"
+
+        mod_count=$(snapctl get modules)
+
+        snapctl set modules=$((mod_count + 1))
+      done
+      snapctl set modified=true        ;;
+    remove-package)
+      for mod in "$@"; do
+        mod_count="$(snapctl get modules)"
+
+        $_caddy "$arg" "$mod"
+
+        snapctl unset "module.${mod##*/}"
+        snapctl set modules=$((mod_count - 1))
+      done
+
+     # If no modules are in the list, we've removed all the modules. Go back to
+     # the normal caddy binary
+      [ "$(snapctl get modules)" -gt 0 ] || {
+        snapctl unset modified; snapctl unset module
+      }
+
+      rm -f "${SNAP_COMMON}/caddy-mod" ;;
+    *) $_caddy "$arg" "$@"             ;;
   esac
 }
 
